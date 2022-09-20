b7e5ca2274cf3d4a65498f76fba96383175cf964
Simon Rofe <simon.rofe@ieee.org>
2022-09-20 12:52:13 +1000

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Simon Rofe on 20/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
