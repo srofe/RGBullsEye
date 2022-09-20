42409274650697e434226cbe3689a5b90494e300
Simon Rofe <simon.rofe@ieee.org>
2022-09-20 20:09:19 +1000

    Update text and add color view

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Color(red: 0.5, green: 0.5, blue: 0.5)
+            Text("R: ??? G: ??? B: ???")
         }
         .padding()
     }
